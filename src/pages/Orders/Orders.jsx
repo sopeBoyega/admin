708679a81c49f2de42b12b4a4f92bbb013f3228c
S._.ope/ <sopeadegboyega@gmail.com>
2024-10-28 19:25:57 +0100

    Added Orders

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import './Orders.css'
+import React, { useState, useEffect } from 'react';
+import './Orders.css';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import { assets } from '../../assets/assets';
 
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const statusHandler =   async (event,orderId)  => {
+const response = await  axios.post (url+"/api/order/status",{
+  orderId,
+  status:event.target.value
+})
+if(response.data.data){
+  await fetchAllOrders();
 }
+  }
 
-export default Orders
+  const fetchAllOrders = async () => {
+    try {
+      const response = await axios.get(url + "/api/order/list");
+      if (response.data.success) {
+        setOrders(response.data.data);
+      } else {
+        toast.error("Error fetching orders");
+      }
+    } catch (error) {
+      toast.error("Failed to fetch orders");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    // Map function returns component after  each iteration unless nothing will be rendered
+    <div className="order add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div className="order-item" key={index}>
+            <img src={assets.parcel_icon} alt="Parcel Icon" />
+            <div>
+              <p className="order-item-food">
+                {order.items.map((item, idx) => {
+                  if (idx === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + " , ";
+                  }
+                })}
+              </p>
+              <p className="order-item-name">
+                {order.address.firstName + " " + order.address.lastName}</p>
+                <div className="order-item-address">
+                  <p>{order.address.street+","}</p>
+                  <p>{order.address.city+" ,"+order.address.state+" ,"+order.address.country+","+order.address.zipcode}</p>
+                </div>
+                <p className="order-item-phone">{order.address.phone}</p>
+            </div>
+            <p>Items  : {order.items.length} </p>
+            <p>${order.amount}</p>
+            <select onChange={(event)=>statusHandler(event,order._id)} value={order.status}>
+              <option value="Food processing">Food processing</option>
+              <option value="Out For Delivery">Out For Delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
